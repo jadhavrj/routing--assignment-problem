e7f8cd65207affa2bc7cfa949eed1aa6b6b2f0fc
RUTURAJPC\rutur <jadhavrj@mail.uc.edu>
2021-01-21 22:56:10 -0500

    Assignment solution

---
+++ src/containers/Course/Course.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Course extends Component {
+    state = {
+        courseTitle: null
+    }
+
+    componentDidMount () {
+        this.parseQueryParams();
+    }
+
+    componentDidUpdate () {
+        this.parseQueryParams();
+    }
+
+    parseQueryParams = () => {
+        const queryParams = new URLSearchParams(this.props.location.search);
+        for (let param of queryParams.entries())
+            if (this.state.courseTitle !== param[1])
+                this.setState({courseTitle: param[1]});
+    }
+
+    render () {
+        console.log(this.props);
+        return (
+            <div>
+                <h1>{this.state.courseTitle}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
+            </div>
+        );
+    }
+}
+
+export default Course;
